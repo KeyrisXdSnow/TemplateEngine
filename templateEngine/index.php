2935aad34d2301435a04f965b0b83cca3c821e9b
Kristina <57489792+KeyrisXdSnow@users.noreply.github.com>
2020-04-11 00:19:29 +0300

    Add files via upload

---
+++ templateEngine/index.php
@@ -0,0 +1,26 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>RegularEx</title>
+</head>
+<body>
+<section>
+    <p> Template html </p>
+    <audio src=\Laba4\templateEngine\files\music.mp3 alt = Cant find file > </audio>
+     <img src=\Laba4\templateEngine\files\sw.jpg alt = Cant find file > 
+     PART2 
+    <br><p>ddddddddddddddddddggfgfgdfgrgfdvssssssssssssssssssfdvvvvvvvvvvvvvvvv dvsv 
+eefadfffffffffffffffffffffffffffffffff
+fgdssgfg</p><br>
+    <img src=\Laba4\templateEngine\files\starWars.jpg alt = Cant find file >
+    <video src=/Laba4/templateEngine/files/Zachar.mp4 alt = Cant find file > </video>
+    <p> Put  </p>
+    <p> Put1  </p>
+    <audio src=\Laba4\templateEngine\music.mp3 alt = 'ff'></audio>;
+
+</section>
+</body>
+</html>
+
+
